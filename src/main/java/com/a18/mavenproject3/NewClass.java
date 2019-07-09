7abce7275fb44b4667d77291f00c96b540c70065
1795698 <1795698@LI-H016-0T20>
2019-07-09 09:55:54 -0400

    creation class terrain

---
+++ src/main/java/com/a18/mavenproject3/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.a18.mavenproject3;
+
+/**
+ *
+ * @author 1795698
+ */
+public class NewClass {
+    
+}
